ec58bc54d8750749377fdf9ccc4ecb6881ce2ed1
Khasar <bendik.warhuus@gmail.com>
2021-01-10 16:29:58 +0100

    minor

--- src/main/java/utility/EventSetTab.java
+++ src/main/java/utility/EventSetTab.java
@@ -1,7 +1,6 @@
 package main.java.utility;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
